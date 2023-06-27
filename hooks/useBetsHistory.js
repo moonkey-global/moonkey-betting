5dd64682540a67c187e85aa4af09042102482a27
adapole <eyassubn@gmail.com>
2023-06-27 13:04:26 +0300

    Fix for production .env, added hooks for bets-history

---
+++ hooks/useBetsHistory.js
@@ -0,0 +1,81 @@
+'use client';
+// import { useEthers } from '@usedapp/core'
+import { gql, useQuery } from '@apollo/client';
+import { ClientContext } from '@/components/ClientProvider';
+import { useContext } from 'react';
+
+const QUERY = `
+  query BetsHistory($first: Int, $where: Bet_filter!) {
+    bets(
+      first: $first,
+      orderBy: createdBlockTimestamp,
+      orderDirection: desc,
+      where: $where,
+      subgraphError: allow
+    ) {
+      id
+      betId
+      amount
+      potentialPayout
+      status
+      isRedeemed
+      odds
+      createdAt: createdBlockTimestamp
+      txHash: createdTxHash
+      outcome {
+        id
+        outcomeId
+        condition {
+          id
+          conditionId
+          wonOutcome {
+            outcomeId
+          }
+          core {
+            address
+            liquidityPool {
+              address
+            }
+          }
+        }
+      }
+      game {
+        id
+        sport {
+          name
+        }
+        league {
+          name
+          country {
+            name
+          }
+        }
+        participants {
+          name
+          image
+        }
+        startsAt
+      }
+    }
+  }
+`;
+
+export default function useBetsHistory() {
+	//   const { account } = useEthers()
+	const { latestAccount, account } = useContext(ClientContext);
+	if (account)
+		return useQuery(
+			gql`
+				${QUERY}
+			`,
+			{
+				variables: {
+					first: 10, // in this tutorial, only 10 bets are loaded. In production, pagination loading should be implemented to avoid heavy requests which can lead to GraphQL errors
+					where: {
+						actor: account?.toLowerCase(),
+					},
+				},
+				skip: !account,
+			}
+		);
+}
---
+++ hooks/useRedeemBet.js
@@ -0,0 +1,40 @@
+'use client';
+import * as ethers from 'ethers';
+import { useContractFunction } from '@usedapp/core';
+import { LP_ADDRESS, CORE_ADDRESS } from '@/lib/constants';
+import { LP_ABI } from '@/lib/lpabi';
+import { ClientContext } from '@/components/ClientProvider';
+import { useState, useContext, useEffect } from 'react';
+
+const lpContract = new ethers.Contract(LP_ADDRESS, LP_ABI);
+
+export default function useRedeemBet({ betId }) {
+	const { account, provider, latestAccount } = useContext(ClientContext);
+	const redeem = async () => {
+		if (!provider) return console.log('Missing provider');
+
+		let LPcontract = new ethers.Contract(
+			LP_ADDRESS,
+			LP_ABI,
+			provider.getSigner()
+		);
+
+		let txn = await LPcontract.withdrawPayout(CORE_ADDRESS, betId, false, {
+			gasLimit: 2100,
+		});
+		console.log('withdrawPayout: ', txn);
+	};
+	// const { state, send } = useContractFunction(lpContract, 'withdrawPayout', {
+	// 	transactionName: 'Approve',
+	// });
+
+	// const redeem = () => {
+	// 	send(CORE_ADDRESS, betId, false);
+	// };
+
+	return {
+		redeem,
+		isRedeeming:
+			state.status === 'PendingSignature' || state.status === 'Mining',
+	};
+}
